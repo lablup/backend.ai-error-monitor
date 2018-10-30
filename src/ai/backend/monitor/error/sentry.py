f6152b67f9df9816cfa86a924ae80b838069912e
Suhwan Jee <suhwan.jee@lablup.com>
2018-10-30 18:34:07 +0900

    Implement error monitor API with Sentry (lablup/backend.ai-manager#107)

---
+++ src/ai/backend/monitor/error/sentry.py
@@ -0,0 +1,29 @@
+import raven
+
+from .base import AbstractErrorMonitor
+
+
+class SentryRavenErrorMonitor(AbstractErrorMonitor):
+
+    def __init__(self):
+        self.sentry = None
+
+    def init(self, config):
+        self.sentry = raven.Client(
+            config.raven_uri,
+            release=raven.fetch_package_version('backend.ai-manager'))
+
+    def capture_exception(self, *args):
+        self.sentry.capture_exception(*args)
+
+    def capture_message(self, msg):
+        self.sentry.capture_message(msg)
+
+    def set_context(self, context):
+        self.sentry.update_context(context)
+
+
+def get_plugin(config):
+    error_monitor = SentryRavenErrorMonitor()
+    error_monitor.init(config)
+    return error_monitor
