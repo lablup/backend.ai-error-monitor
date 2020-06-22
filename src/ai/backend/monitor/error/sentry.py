51889a337aed23c49d9dce51bf7180b9e5385e47
Joongi Kim <joongi@lablup.com>
2020-06-22 16:50:56 +0900

    Update for the plugin API 2.0 and the Sentry SDK replacing Raven

--- src/ai/backend/monitor/error/sentry.py
+++ src/ai/backend/monitor/error/sentry.py
@@ -1,41 +1,33 @@
-import raven
+from typing import (
+    Any,
+    Mapping,
+    Union,
+)
 
-from ai.backend.common.monitor import AbstractErrorMonitor
+import sentry_sdk
+
+from ai.backend.common.plugin.monitor import AbstractErrorReporterPlugin
+from . import __version__
 
 
-class SentryRavenErrorMonitor(AbstractErrorMonitor):
+class SentryErrorMonitor(AbstractErrorMonitor):
 
-    def __init__(self):
-        self.sentry = None
+    async def init(self) -> None:
+        sentry_sdk.init(
+            self.plugin_config['dsn'],
+            release=__version__,
+        )
 
-    def init(self, config):
-        self.sentry = raven.Client(
-            config.raven_uri,
-            release=raven.fetch_package_version(config.app_name))
-
-    def capture_exception(self, *args):
-        self.sentry.captureException(*args)
-
-    def capture_message(self, msg):
-        self.sentry.captureMessage(msg)
-
-    def set_context(self, context):
-        self.sentry.user_context(context)
-
-    def __enter__(self):
-        return self
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
+    async def cleanup(self) -> None:
         pass
 
+    async def capture_exception(self, exc_instance: Exception = None) -> None:
+        sentry_sdk.capture_exception(exc_instance)
 
-def add_plugin_args(parser):
-    parser.add('--raven-uri', env_var='RAVEN_URI',
-               type=str, default=None,
-               help='The sentry.io event report URL with DSN.')
+    async def capture_message(self, msg: str) -> None:
+        sentry_sdk.capture_message(msg)
 
-
-def get_plugin(config):
-    error_monitor = SentryRavenErrorMonitor()
-    error_monitor.init(config)
-    return error_monitor
+    async def update_plugin_config(self, new_plugin_config: Mapping[str, Any]) -> None:
+        self.plugin_config = self.new_plugin_config
+        await self.cleanup()
+        await self.init()
